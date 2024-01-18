b4425ef0e4f8bb0d1068a16da042c0da82fe9378
grapefruitsattack <teachcactustoswim+1@gmail.com>
2024-01-18 19:17:44 +0900

    ユニット・アイドルページにメタデータ追加

--- src/app/idol/[id]/page.tsx
+++ src/app/idol/[id]/page.tsx
@@ -1,17 +1,25 @@
-
-import songMaster from '../../../data/singingMaster.json';
+import { Metadata } from 'next'
 import dynamic from "next/dynamic";
+import singingMaster from '../../../data/singingMaster.json';
+import albumMaster from '../../../data/albumMaster.json';
 
 export function generateStaticParams() {
-  const idols = songMaster.filter(data=>data.personFlg===1);
+  const idols = singingMaster.filter(data=>data.personFlg===1);
   return idols.map((e)=>{
     return {id: e.singingInfoId}
   });
   }
-  const IdolPage = dynamic(() => import("../../../features/app/idol/IdolPage"), { ssr: false });
-  export default function Page({ params }: { params: { id: string } }) {
+  export async function generateMetadata({ params }: { params: { id: string } }): Promise<Metadata> {
+    const idolName: string = singingMaster.find((data)=>data.singingInfoId=== params.id)?.singingInfoName||'';
+    return { 
+      title: `${idolName} ${'\u00a0'}|${'\u00a0\u00a0'}サーチサイコー` ,
+      description: `「${idolName}」の楽曲情報・サブスク配信状況をチェック！ |  サーチサイコー`
+    };
+  }
+const IdolPage = dynamic(() => import("../../../features/app/idol/IdolPage"), { ssr: false });
+export default function Page({ params }: { params: { id: string } }) {
 
-    return (
-      <IdolPage id = {params.id} />
-      );
-    }
+  return (
+    <IdolPage id = {params.id} />
+    );
+  }
--- src/app/unit/[id]/page.tsx
+++ src/app/unit/[id]/page.tsx
@@ -1,17 +1,24 @@
-
-import songMaster from '../../../data/singingMaster.json';
+import { Metadata } from 'next'
+import singingMaster from '../../../data/singingMaster.json';
 import dynamic from "next/dynamic";
 
 export function generateStaticParams() {
-  const idols = songMaster.filter(data=>data.personFlg===0);
+  const idols = singingMaster.filter(data=>data.personFlg===0);
   return idols.map((e)=>{
     return {id: e.singingInfoId}
   });
-  }
-  const UnitPage = dynamic(() => import("../../../features/app/unit/UnitPage"), { ssr: false });
-  export default function Page({ params }: { params: { id: string } }) {
+}
+export async function generateMetadata({ params }: { params: { id: string } }): Promise<Metadata> {
+  const unitName: string = singingMaster.find((data)=>data.singingInfoId=== params.id)?.singingInfoName||'';
+  return { 
+    title: `${unitName} ${'\u00a0'}|${'\u00a0\u00a0'}サーチサイコー` ,
+    description: `「${unitName}」の楽曲情報・サブスク配信状況をチェック！ |  サーチサイコー`
+  };
+}
+const UnitPage = dynamic(() => import("../../../features/app/unit/UnitPage"), { ssr: false });
+export default function Page({ params }: { params: { id: string } }) {
 
-    return (
-      <UnitPage id = {params.id} />
-      );
-    }
+  return (
+    <UnitPage id = {params.id} />
+    );
+  }
