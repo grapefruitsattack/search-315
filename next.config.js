60faedc19ee31a1029a0fce77431675d0e2a7db7
grapefruitsattack <teachcactustoswim+1@gmail.com>
2024-03-31 22:47:33 +0900

    PWA対応

--- next.config.js
+++ next.config.js
@@ -1,26 +1,38 @@
 /** @type {import('next').NextConfig} */
 const nextBuildId = require("next-build-id");
-const nextConfig = {
-    experimental: {
-        scrollRestoration: true,
-        swcMinify: false,
-        appDir:true
-    },
-    trailingSlash: true,
-    assetPrefix: process.env.BASE_PATH || '',
-    basePath: process.env.BASE_PATH || '',
-    output: 'export',
-    generateBuildId: async () => {
-      return nextBuildId({ dir: __dirname });
-    }
-}
+// const nextConfig = {
+//     experimental: {
+//         scrollRestoration: true,
+//         swcMinify: false,
+//         appDir:true
+//     },
+//     trailingSlash: true,
+//     assetPrefix: process.env.BASE_PATH || '',
+//     basePath: process.env.BASE_PATH || '',
+//     output: 'export',
+//     generateBuildId: async () => {
+//       return nextBuildId({ dir: __dirname });
+//     }
+// }
 
 const withPWA = require('next-pwa')({
-  dest: "public",
+  dest: 'public',
   register: true,
   skipWaiting: true,
 })
 
 module.exports = withPWA({
-  nextConfig
-});
+  experimental: {
+      scrollRestoration: true,
+      swcMinify: false,
+      appDir:true
+  },
+  trailingSlash: true,
+  assetPrefix: process.env.BASE_PATH || '',
+  basePath: process.env.BASE_PATH || '',
+  output: 'export',
+  generateBuildId: async () => {
+    return nextBuildId({ dir: __dirname });
+  },
+  reactStrictMode: true,
+})
