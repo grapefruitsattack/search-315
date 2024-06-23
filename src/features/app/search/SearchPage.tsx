b42bc1b464c913a37ff2a2e69ab2fd8e4d89aba2
grapefruitsattack <teachcactustoswim+1@gmail.com>
2024-06-23 22:18:22 +0900

    エラー解消　不要ファイルの削除

--- src/features/app/search/SearchPage.tsx
+++ src/features/app/search/SearchPage.tsx
@@ -1,29 +1,11 @@
 'use client'
 import { useSearchParams } from 'next/navigation'
 import CommonPage from "../../common/components/CommonPage";
-import TabComponent from "../../common/components/TabComponent";
 import SearchPageSong from "./components/SearchPageSong";
-import SearchPageAlbum from "./components/SearchPageAlbum";
 import {SearchModal} from "./components/SearchModal";
-
-import {
-  Button,
-  Modal,
-  ModalBody,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  useDisclosure, 
- } from "@chakra-ui/react";
 
 export default function SearchPage() {
     const searchParams = useSearchParams();
-    const search :string[] | undefined = searchParams.get('q')?.split(' ');
-    const order :string = searchParams.get('order') || 'desc';
-
-   
- const { isOpen, onClose, onOpen } = useDisclosure();
-
 
     return (
       <CommonPage>
@@ -34,7 +16,7 @@ export default function SearchPage() {
       </div>
       <SearchPageSong />
     
-    </section>
+      </section>
       </CommonPage>
       );
 }
