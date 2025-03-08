2be8e55c4596a07e2770a38349873c50e65240d8
grapefruitsattack <teachcactustoswim+1@gmail.com>
2025-03-08 22:31:22 +0900

    Auth.js Supabase接続

--- auth.ts
+++ auth.ts
@@ -1,11 +1,16 @@
 import NextAuth from "next-auth";
 import type { NextAuthConfig } from "next-auth";
 import Google from "next-auth/providers/google";
+import { SupabaseAdapter } from "@auth/supabase-adapter"
 
 const authConfig: NextAuthConfig = {
     providers: [
       Google,
     ],
+    adapter: SupabaseAdapter({
+      url: process.env.SUPABASE_URL||'',
+      secret: process.env.SUPABASE_SERVICE_ROLE_KEY||'',
+    }),
    callbacks: {
      async jwt({ token, user, account }) {
        if (user && account?.id_token) {
