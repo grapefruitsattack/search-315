9b377559988dcbcfa6bd1de877c4195d6069e77f
grapefruitsattack <200chome@gmail.com>
2026-08-11 14:47:48 +0900

    ログインボタン　読み込み中表示

--- src/features/common/components/common/UserButton.tsx
+++ src/features/common/components/common/UserButton.tsx
@@ -1,34 +1,47 @@
 'use client'
 import React from "react"
-import CommonPage from "./CommonPage";
 import { useSession } from "@/auth-client";
-import { headers } from "next/headers";
 import {SignIn,SignOut} from "@/features/management/auth/SignIn";
 import Link from "next/link";
+import { Skeleton } from "@/components/ui/skeleton"
 import { CircleUserRound } from "lucide-react";
+
 
 export default function UserButton() {
 
-  const { data } = useSession();
+  const { data, isPending } = useSession();
+
+
+  if (isPending) {
+    return (
+      <div>
+        <div className="flex pc:flex-row flex-col items-center">
+          <Skeleton className="h-[40px] w-[40px] rounded-xl mx-auto pc:mx-1" />
+          <Skeleton className="h-5 w-20 mt-1 pc:mt-0" />
+        </div>
+      </div>
+    )
+  }
+
   const login: boolean = data?.user?true:false;
 
-    return (
-        <section className="">
-          
-          {login
-              ?
-              <Link className='flex pc:flex-row flex-col hover:text-zinc-800 text-zinc-500'
-                href={`/mypage`}
-                rel="noopener noreferrer"
-              >
-              <CircleUserRound className=' h-[40px] w-[40px] mx-auto pc:mx-1 ' />
-              <p className='text-xs tablet:text-sm pc:my-auto pc:text-lg'>{'マイページ'}</p>
-              </Link>
-              :
-              <SignIn></SignIn>
-            }
-          
-       
-        </section>
-      );
+  return (
+      <div className="">
+        
+        {login
+            ?
+            <Link className='flex pc:flex-row flex-col hover:text-zinc-800 text-zinc-500'
+              href={`/mypage`}
+              rel="noopener noreferrer"
+            >
+            <CircleUserRound className=' h-[40px] w-[40px] mx-auto pc:mx-1 ' />
+            <p className='text-xs tablet:text-sm pc:my-auto pc:text-lg'>{'マイページ'}</p>
+            </Link>
+            :
+            <SignIn></SignIn>
+          }
+        
+      
+      </div>
+  );
   }
