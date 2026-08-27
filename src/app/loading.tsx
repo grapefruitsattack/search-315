834e1f1ac0b0e1463778967334c6f62d2dbf11ec
grapefruitsattack <200chome@gmail.com>
2026-08-27 22:02:53 +0900

    loading表示

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,3 +1,12 @@
 export default function Loading() {
-    return <h1>loading...</h1>
+    return (
+    <div className="flex w-full h-full">
+      <div className='flex w-fit h-fit mx-auto mt-24
+        font-black text-2xl text-gray-400
+        animate-pulse'
+      >
+        {'Loading...'}
+      </div>
+    </div>
+    )
   }
