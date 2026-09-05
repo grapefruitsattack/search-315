cc3a4c201704142c528d3c25a15502dbbdcdd5be
grapefruitsattack <200chome@gmail.com>
2026-09-05 18:51:28 +0900

    曲ページ　歌詞読み込み中は歌詞共有モーダルを無効

--- src/features/app/song/components/SongContent.tsx
+++ src/features/app/song/components/SongContent.tsx
@@ -240,18 +240,23 @@ export default function SongContent({ result }: { result: SongMaster }) {
               ?<></>
               :
               <button 
-                className='
-                  flex py-2 px-2 gap-1 tablet:px-5 rounded-full bg-zinc-100 items-center w-fit h-fit
+                className={`
+                  flex py-2 px-2 gap-1 tablet:px-5 rounded-full  items-center w-fit h-fit
                   font-mono text-xs mobileL:text-sm tablet:text-base 
                   transition-all duration-300
-                  hover:ring-2 hover:ring-zinc-600 hover:ring-offset-2 hover:bg-zinc-200
-                  active:scale-90
-                ' 
+                  ${isLoading
+                    ?`bg-zinc-50 text-zinc-400 `
+                    :`bg-zinc-100 
+                      hover:ring-2 hover:ring-zinc-600 hover:ring-offset-2 hover:bg-zinc-200 
+                      active:scale-90`
+                  }
+                `}
                 onClick={(lyricShareModalDisclosure.onOpen)}
+                disabled={isLoading}
               >
                 <LyricShareModal song={result} disclosure={lyricShareModalDisclosure} targetRows={{ startRow: 0, endRow: 0 }} lyric={lyric} />
                 {/* Google Fonts Icons */}
-                <svg className="fill-zinc-600 w-[18px] h-[18px] mobileL:w-[24px] mobileL:h-[24px]" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
+                <svg className={`${isLoading?'fill-zinc-400':'fill-zinc-600'} w-[18px] h-[18px] mobileL:w-[24px] mobileL:h-[24px]`} xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
                   <path d="M240-80q-33 0-56.5-23.5T160-160v-400q0-33 23.5-56.5T240-640h120v80H240v400h480v-400H600v-80h120q33 0 56.5 23.5T800-560v400q0 33-23.5 56.5T720-80H240Zm200-240v-447l-64 64-56-57 160-160 160 160-56 57-64-64v447h-80Z"/>
                 </svg>
                 {'歌詞を共有'}
